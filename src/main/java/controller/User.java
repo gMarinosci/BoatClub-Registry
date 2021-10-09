4d46e561677abc098350a4036778a4f868446ed7
Raya Wagner <rw222hi@student.lnu.se>
2021-10-09 22:35:14 +0200

    run menu

--- src/main/java/controller/User.java
+++ src/main/java/controller/User.java
@@ -8,12 +8,45 @@ import java.util.ArrayList;
 
 public class User {
 
-  public boolean active (UserInterface view) {
-    while (view.mainMenu()) {
+  public boolean run(int n) {
 
+    switch (n) {
+      case 1:
+        //Verbose list
+        ui.showMemberListVerbose;
+      case 2:
+        //Compact List
+        ui.showMemberListCompact;
+      case 3:
+        //A specific member's information
+        String name = ui.promptMemberName();
+        String lastname = ui.promptMemberLastName();
+        menu.showSpecificMemberInfo(name, lastname);
+      case 4:
+        //Add a new member
+        ui.provideMemberFirstName();
+        String name4 = ui.promptMemberName();
+        ui.provideMemberLastName();
+        String lastname4 = ui.promptMemberLastName();
+        ui.provideMemberPersonalNumber();
+        int personalNumber = ui.promptMemberPersonalNumber();
+        menu.addNewMember(name4, lastname4, personalNumber);
+      case 5:
+        //Delete a member
+        String ID = ui.promptID();
+        menu.deleteMember(ID);
+      case 6:
+        //Change a member's information
+        String ID6 = ui.promtID();
+        menu.changeMemberInformation(ID6);
+      case 7:
+        //Register a new boat
+      case 8:
+        //Delete a boat
+      case 9:
+        //Change a boat's information
     }
-    return false;
-  };
+  }
 
   public ArrayList<Member> createData(){
     ArrayList<Member> membersList = new ArrayList<Member>();
