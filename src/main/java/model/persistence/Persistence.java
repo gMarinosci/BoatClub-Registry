81ed2dfec8acdccded35dfac092430bfc2d029ce
Raya Wagner <rw222hi@student.lnu.se>
2021-10-09 22:43:31 +0200

    persistence preparation

---
+++ src/main/java/model/persistence/Persistence.java
@@ -0,0 +1,15 @@
+package model.persistence;
+
+//manually prepared and added member data can be found in User.java when the program is started
+//all changes performed on the memberList are currently not saved when the program ends
+
+public class Persistence {
+    //preparation for higher grade requirements, persistent loading and saving of data
+
+    void load(){
+        //read JSON or other file
+    }
+    void save(){
+        //write changed memberList in file
+    }
+}
